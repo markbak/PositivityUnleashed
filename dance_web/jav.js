020f8bdb082f91b353c733239871bf9aae76bd61
Askornafel <36460788+Askornafel@users.noreply.github.com>
2019-11-13 20:09:12 -0500

    Add files via upload

---
+++ dance_web/jav.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	console.log('the doc is ready');
+	
+	$('#home').fadeIn(7000);
+	$('#b1').fadeIn(5000);
+	$('#b2').fadeIn(7000);
+	$('#b3').fadeIn(10000);
+	$('#b4').fadeIn(12000);
+	
+
+});
